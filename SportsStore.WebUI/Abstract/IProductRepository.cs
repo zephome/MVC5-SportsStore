5c1f8c64d76edcb2b5bb76bb664f4fb272529fa1
zephome@gmail.com <zephome@gmail.com>
2016-09-07 16:35:35 +0900

    도메인 모델 작성 (p.216)

---
+++ SportsStore.WebUI/Abstract/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.WebUI.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Produts { get; set; }
+    }
+}
--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,5 +1,9 @@
-﻿using Ninject;
+﻿using Moq;
+using Ninject;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Abstract;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -30,6 +34,15 @@ namespace SportsStore.WebUI.Infrastructure
         private void AddBinding()
         {
             // 바인딩 정보
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Produts).Returns(new List<Product>
+            {
+                new Product { Name = "Football", Price = 25 },
+                new Product { Name = "Surf board", Price = 179 },
+                new Product { Name = "Running shoes", Price = 95 }
+            });
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
